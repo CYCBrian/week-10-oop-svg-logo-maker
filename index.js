956e76eb80460f212af4fbbe8321678a7c6a0506
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2023-12-07 17:07:26 -0500

    3 and 5 - ADDED question array and generateSvg function

---
+++ index.js
@@ -0,0 +1,40 @@
+const inquirer = require('inquirer')
+const fs = require ('fs')
+// TODO : create shapes in shapes.js and link to here
+
+const svgPrompts = [
+    {
+        type:"list",
+        name:"shapeType",
+        message:"Select shape of logo.",
+        choices:["Circle", "Square", "Triangle"]
+    },
+    {
+        type:"input",
+        name:"shapeColor",
+        message:"Input color of logo (color keyword or hexadecimal number)."
+    },
+    {
+        type:"input",
+        name:"textInput",
+        message:"Input text of logo (max 3 characters)."
+    },
+    {
+        type:"input",
+        name:"textColor",
+        message:"Input color of text (color keyword or hexadecimal number)."
+    }
+]
+
+function writeToFile(filename, data){
+// TODO: Function to create file with svg logo.
+}
+
+function generateSvg(){
+    inquirer.prompt(svgPrompts).then((data) => {
+        console.log(data)
+        writeToFile("logo.svg", data)
+    })
+}
+
+generateSvg()
