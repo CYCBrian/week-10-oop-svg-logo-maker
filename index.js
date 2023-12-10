ca47264c54102ff46e918e81203d3580dc166bf5
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2023-12-10 18:11:14 -0500

    4.ADDED createSVGFILE function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Circle, Square, Triangle } = require('./shapes');
+const { Circle, Square, Triangle } = require('./lib/shapes');
 
 const svgPrompts = [
     {
@@ -26,14 +26,40 @@ const svgPrompts = [
     }
 ]
 
-function writeToFile(filename, data){
-// TODO: Function to create file with svg logo.
-}
+function createSvgFile(filename, data) {
+    let shape;
+    if (data.shapeType === "Circle") {
+        shape = new Circle();
+    } else if (data.shapeType === "Square") {
+        shape = new Square();
+    } else if (data.shapeType === "Triangle") {
+        shape = new Triangle();
+    }  
+    shape.setColor(data.shapeColor);
+  
+    const svgString = `<svg version="1.1" width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+        ${shape.render()}
+        <text x="150" y="150" font-size="100" text-anchor="middle" dominant-baseline="middle" fill="${data.textColor}">${data.textInput}</text>
+    </svg>`;
+  
+    fs.writeFile(filename, svgString, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("Successfully generated logo!");
+        }
+    });
+  }
 
 function generateSvg(){
     inquirer.prompt(svgPrompts).then((data) => {
-        console.log(data)
-        writeToFile("logo.svg", data)
+        if (data.textInput.length > 3){
+            console.log("Please enter a maximum of 3 characters.")
+            generateSvg()
+        } else {
+            console.log(data)
+            createSvgFile("logo.svg", data)
+        }
     })
 }
 
